fde7b8abfa2500b64969f3a083bc9d3d1551107a
prathit12 <56720633+prathit12@users.noreply.github.com>
2022-01-04 12:18:20 +0530

    Add files via upload

---
+++ Calculator/src/Calculator/Calculator.java
@@ -0,0 +1,10 @@
+package Calculator;
+public class Calculator {
+	public static void main(final String args[]) {
+		final int x = 10, y = 20;
+		int z;
+		z=x+y;
+		System.out.println("answer is "+z);
+	}
+
+}
